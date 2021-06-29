85d77a9b9105f02a6e30456fdf2847da012856a6
SungJunEun <60536942+SungJunEun@users.noreply.github.com>
2021-06-29 15:38:35 +0000

    event handle

--- script.js
+++ script.js
@@ -14,8 +14,24 @@ function createHTMLstring(item) {
   <span>${item.gender} ${item.size} size</span>
   </li>`; // return 옆에서 바로 시작해야 알아먹음
 }
+function setEventlistners(items) {
+  const logo = document.querySelector('.logo');
+  const btn = document.querySelector('nav');
+  logo.addEventListener('click', ()=> displayItems(items));
+  btn.addEventListener('click', event => onButtonClick(event, items));
+}
+function onButtonClick(event, items) {
+  const key = event.target.dataset.key;
+  const value = event.target.dataset.value;
+  if(key == null || value == null) {
+    return;
+  }
+  const filtereditems = items.filter(item => item[key]== value);
+  displayItems(filtereditems);
+}
 loadItems()
   .then(items => {
     displayItems(items);
+    setEventlistners(items);
   })
   .catch(console.log);
